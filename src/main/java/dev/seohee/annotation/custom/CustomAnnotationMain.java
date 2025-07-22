eca6123537b4331163126ef97c4b5093f579b3df
moon-three <moondal366@gmail.com>
2025-07-22 16:18:55 +0900

    [Annotation, Reflection] Custom Annotation: 실제 작업 전 기본형

---
+++ src/main/java/dev/seohee/annotation/custom/CustomAnnotationMain.java
@@ -0,0 +1,8 @@
+package dev.seohee.annotation.custom;
+
+public class CustomAnnotationMain {
+    public static void main(String[] args) {
+        Car car= new Car("BMW", "2021");
+        System.out.println(car);
+    }
+}
